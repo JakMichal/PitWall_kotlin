c62ba15f3d4335a9d1cd8f8b30059bb398f33e72
150cmMisko <TVOJ_NO_REPLY@users.noreply.github.com>
2026-03-29 16:21:41 +0200

    circuit image

--- app/src/main/java/com/example/pitwall/MainActivity.kt
+++ app/src/main/java/com/example/pitwall/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,6 +13,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +25,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -110,6 +114,16 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             Text(text = "Monaco Grand Prix", fontSize = 35.sp, color = Color.White)
             Text(text = "Remaining", fontSize = 30.sp, color = Color.Red)
             Text(text = "00:09:45:36", fontSize = 30.sp, color = Color.Red)
+            Image(
+                modifier = Modifier
+                    .background(Color.Transparent)
+                    .padding(top = 40.dp)
+                    .size(250.dp),
+                painter = painterResource(R.drawable.monaco_circuit),
+                contentDescription = "Monaco circuit",
+                colorFilter = ColorFilter.tint(Color.White),
+
+            )
         }
     }
 }
