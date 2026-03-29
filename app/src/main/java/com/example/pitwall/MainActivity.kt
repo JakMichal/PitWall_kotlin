050d3485a3d605a63bfc30a54f83814648908e05
150cmMisko <TVOJ_NO_REPLY@users.noreply.github.com>
2026-03-29 16:28:33 +0200

    circuit image

--- app/src/main/java/com/example/pitwall/MainActivity.kt
+++ app/src/main/java/com/example/pitwall/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,14 +20,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.pitwall.ui.theme.PitWallTheme
 
 class MainActivity : ComponentActivity() {
@@ -107,7 +100,7 @@ fun ChangeScreen(
 
 @Preview(showBackground = true)
 @Composable
-fun HomeScreenPreview(modifier: Modifier = Modifier) {
+fun HomeScreenPreview() {
     PitWallTheme {
         var activeScreen by remember { mutableStateOf("Home") }
         ChangeScreen(
