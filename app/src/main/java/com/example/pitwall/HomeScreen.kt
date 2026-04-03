6b1f9fff43894eb6ead6f84a1f4fc725ad52135e
150cmMisko <TVOJ_NO_REPLY@users.noreply.github.com>
2026-04-03 20:56:01 +0200

    homeScreen better2

--- app/src/main/java/com/example/pitwall/HomeScreen.kt
+++ app/src/main/java/com/example/pitwall/HomeScreen.kt
@@ -1,21 +1,34 @@
 package com.example.pitwall
 
+import android.R.color.transparent
+import android.R.id.bold
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -24,23 +37,41 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pitwall.ui.theme.PitWallBackground
 import com.example.pitwall.ui.theme.PitWallTheme
+import kotlin.math.round
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(modifier: Modifier = Modifier, onScreenChange: (String) -> Unit) {
     LazyColumn(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(),
+        contentPadding = PaddingValues(bottom = 100.dp)
     ) {
         item { NextRace() }
-        item { SeasonCalendar() }
+        item {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                ToScreenButton("Schedule", onScreenChange, modifier.weight(1f))
+                ToScreenButton("All Result", onScreenChange, modifier.weight(1f))
+            }
+        }
         item { DriverChampionship() }
         item { ContructorChampionship() }
     }
@@ -48,133 +79,199 @@ fun HomeScreen(modifier: Modifier = Modifier) {
 
 @Composable
 fun NextRace(modifier: Modifier = Modifier) {
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(text = "NEXT RACE", fontSize = 13.sp, color = Color.Red)
-            Card(
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.surface
-                ),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                Column(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .padding(top = 15.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-
-                ) {
-                    Column(
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(start = 16.dp, bottom = 10.dp),
-                    ) {
-                        Text(text = "Monaco Grand Prix", fontSize = 30.sp, color = Color.White)
-                        Row() {
-                            Text(text = "Circuit de Monaco", fontSize = 15.sp, color = Color.DarkGray)
-                            Text(text = " • ", fontSize = 15.sp, color = Color.DarkGray)
-                            Text(text = "Round 8", fontSize = 15.sp, color = Color.DarkGray)
-                        }
-                    }
-                    Text(
-                        text = "Remaining",
-                        fontSize = 30.sp,
-                        color = Color.Red,
-                        textAlign = TextAlign.Center
-                    )
-                    Row() {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(text = "02", fontSize = 30.sp, color = Color.Red)
-                            Text(text = "days", fontSize = 15.sp, color = Color.DarkGray)
-                        }
-                        Text(" : ", fontSize = 30.sp, color = Color.Red)
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(text = "15", fontSize = 30.sp, color = Color.Red)
-                            Text(text = "hours", fontSize = 15.sp, color = Color.DarkGray)
-                        }
-                        Text(" : ", fontSize = 30.sp, color = Color.Red)
-
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(text = "38", fontSize = 30.sp, color = Color.Red)
-                            Text(text = "min", fontSize = 15.sp, color = Color.DarkGray)
-                        }
-                        Text(" : ", fontSize = 30.sp, color = Color.Red)
-
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        ) {
-                            Text(text = "22", fontSize = 30.sp, color = Color.Red)
-                            Text(text = "sec", fontSize = 15.sp, color = Color.DarkGray)
-                        }
-
-                    }
-
-                    Image(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp, 16.dp),
-                        contentScale = ContentScale.Fit,
-                        painter = painterResource(R.drawable.monaco_circuit),
-                        contentDescription = "Monaco circuit",
-                        colorFilter = ColorFilter.tint(Color.White),
-
-                        )
-                }
-            }
-        }
-}
-
-@Composable
-fun SeasonCalendar(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+            .padding(16.dp)
     ) {
-        Text(text = "SEASON CALENDAR", fontSize = 13.sp, color = Color.Red)
-        LazyRow(
-            modifier = modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.spacedBy(15.dp)
+        Text(text = "NEXT RACE", fontSize = 13.sp, color = Color.Red)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(305.dp)
+                .clip(RoundedCornerShape(10.dp))
         ) {
-            items(raceList) {
-                currentRace -> RaceCard(race = currentRace, modifier = Modifier.fillParentMaxWidth())
-            }
+            NextRaceCard()
+            NextRaceBackground()
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(top = 15.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                NextRaceName()
+                NextRaceCircuit()
+                NextRaceTimer()
+             }
         }
     }
 }
 
 @Composable
-fun RaceCard(race: Race, modifier: Modifier = Modifier) {
+fun NextRaceCard() {
     Card(
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
+            containerColor = Color.Transparent
         ),
-        modifier = modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(10.dp))
+            .size(305.dp)
+            .paint(
+                painterResource(R.drawable.monaco_background),
+                contentScale = ContentScale.Crop,
+            )
+
+    ) { }
+}
+
+@Composable
+fun NextRaceBackground() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(305.dp)
+            .background(Color.Black.copy(alpha = 0.7f))
+    ) { }
+}
+
+@Composable
+fun NextRaceName() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp, bottom = 10.dp),
+    ) {
+        Text(
+            text = "Monaco Grand Prix",
+            fontSize = 30.sp,
+            fontFamily = FontFamily.Cursive,
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+        )
+        Row() {
+            Text(
+                text = "Circuit de Monaco",
+                fontSize = 15.sp,
+                color = Color.LightGray
+            )
+            Text(text = " • ", fontSize = 15.sp, color = Color.LightGray)
+            Text(text = "Round 8", fontSize = 15.sp, color = Color.LightGray)
+        }
+    }
+}
+
+@Composable
+fun NextRaceTimer() {
+    Box(modifier = Modifier
+        .background(
+        brush = Brush.verticalGradient(
+            colors = listOf(Color.Transparent, Color.Black)
+        )
+    )
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(start = 16.dp, end = 16.dp, top = 8.dp, bottom = 8.dp)
+                .padding(start = 16.dp),
         ) {
-            Text(text = race.country, fontSize = 13.sp, color = Color.Gray)
-            Text(text = race.name, fontSize = 16.sp, color = Color.LightGray)
-            Row() {
-                Text(text = race.date, fontSize = 14.sp, color = Color.Gray)
-                Text(text = " • ", fontSize = 15.sp, color = Color.DarkGray)
-                Text(text = race.time, fontSize = 14.sp, color = Color.Gray)
+            Text(
+                text = "Remaining",
+                fontSize = 30.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 5.dp)
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "02",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Red
+                    )
+                    Text(text = "days", fontSize = 15.sp, color = Color.LightGray)
+                }
+                Text(" : ", fontSize = 30.sp, fontWeight = FontWeight.Bold, color = Color.Red)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "15",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Red
+                    )
+                    Text(text = "hours", fontSize = 15.sp, color = Color.LightGray)
+                }
+                Text(" : ", fontSize = 30.sp, fontWeight = FontWeight.Bold, color = Color.Red)
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "38",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Red
+                    )
+                    Text(text = "min", fontSize = 15.sp, color = Color.LightGray)
+                }
+                Text(
+                    text = " : ",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red
+                )
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "22",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Red
+                    )
+                    Text(text = "sec", fontSize = 15.sp, color = Color.LightGray)
+                }
+
             }
         }
     }
 }
+
+@Composable
+fun NextRaceCircuit() {
+    Image(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp, 16.dp),
+        contentScale = ContentScale.Fit,
+        painter = painterResource(R.drawable.monaco_circuit),
+        contentDescription = "Monaco circuit",
+        colorFilter = ColorFilter.tint(Color.White),
+    )
+}
+
+@Composable
+fun ToScreenButton(change:String, onScreenChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    OutlinedButton(
+        onClick = { onScreenChange(change) },
+        modifier = modifier,
+        border = BorderStroke(1.dp, Color.DarkGray),
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = Color.LightGray,
+            containerColor = MaterialTheme.colorScheme.surface
+        )
+    ) {
+        Text(change)
+    }
+}
+
 @Composable
 fun DriverChampionship(modifier: Modifier = Modifier) {
     Column(
@@ -187,7 +284,8 @@ fun DriverChampionship(modifier: Modifier = Modifier) {
             colors = CardDefaults.cardColors(
                 containerColor = MaterialTheme.colorScheme.surface
             ),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, color = Color.DarkGray)
         ) {
             Column(
                 modifier = Modifier
@@ -220,7 +318,7 @@ fun DriverChampionship(modifier: Modifier = Modifier) {
                         progress = {driver.points.toFloat() / maxPoints.toFloat()},
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 3.dp ,bottom = 2.dp),
+                            .padding(top = 3.dp, bottom = 2.dp),
                         color = MaterialTheme.colorScheme.primary,
                         trackColor = Color.DarkGray,
                         drawStopIndicator = {}
@@ -242,7 +340,9 @@ fun ContructorChampionship(modifier: Modifier = Modifier) {
             colors = CardDefaults.cardColors(
                 containerColor = MaterialTheme.colorScheme.surface
             ),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            border = BorderStroke(1.dp, color = Color.DarkGray)
+
         ) {
             Column(
                 modifier = Modifier
--- app/src/main/java/com/example/pitwall/MainActivity.kt
+++ app/src/main/java/com/example/pitwall/MainActivity.kt
@@ -4,25 +4,44 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pitwall.ui.theme.PitWallBackground
 import com.example.pitwall.ui.theme.PitWallTheme
 
@@ -32,7 +51,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PitWallTheme {
-                var activeScreen by remember { mutableStateOf("Home") }
+                var activeScreen by rememberSaveable { mutableStateOf("Home") }
                 ChangeScreen(
                     activeScreen = activeScreen,
                     onScreenChange = { activeScreen = it }
@@ -49,49 +68,93 @@ fun ChangeScreen(
     modifier: Modifier = Modifier,
     onScreenChange: (String) -> Unit,
 ) {
-    Scaffold { innerPadding ->
-        Column(
-            modifier.fillMaxSize()
-                .background(color = PitWallBackground)
-                .padding(innerPadding),
-        ) {
-            Box(
-                modifier.weight(1f)
+    Scaffold(
+        containerColor = PitWallBackground,
+        bottomBar = {
+            NavigationBar(
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 2.dp, end = 15.dp, bottom = 10.dp)
+                    .border(1.dp, Color.DarkGray, RoundedCornerShape(20.dp))
+                    .clip(RoundedCornerShape(20.dp))
+                    .height(80.dp),
+                containerColor = MaterialTheme.colorScheme.surface,
+
             ) {
-                when (activeScreen) {
-                    "Home" -> HomeScreen()
-                    "Result" -> ResultScreen()
-                    "Stats" -> StatsScreen()
-                    "Favorite" -> FavoriteScreen()
-                }
-            }
-            Row(
-                modifier.fillMaxWidth()
-                    .background(color = Color(0xff212121)),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button(
-                    onClick = {
-                        onScreenChange("Home")
-                    }
-                ) { Text("Home") }
-                Button(
-                    onClick = {
-                        onScreenChange("Result")
-                    }
-                ) { Text("Result") }
-                Button(
-                    onClick = {
-                        onScreenChange("Stats")
-                    }
-                ) { Text("Stats") }
-                Button(
-                    onClick = {
-                        onScreenChange("Favorite")
-                    }
-                ) { Text("Favorite") }
+                NavigationBarItem(
+                    selected = activeScreen == "Home",
+                    onClick = { onScreenChange("Home") },
+                    icon = { Icon(Icons.Default.Home, contentDescription = "Home", modifier = Modifier.size(20.dp)) },
+                    label = { Text("Home") },
+                    colors = NavigationBarItemDefaults.colors (
+                        indicatorColor = Color.Red,
+                    ),
+                )
+                NavigationBarItem(
+                    selected = activeScreen == "All Result",
+                    onClick = { onScreenChange("All Result") },
+                    icon = { Icon(painter = painterResource(R.drawable.result), contentDescription = "Result", modifier = Modifier.size(20.dp)) },
+                    label = { Text("Result") },
+                    colors = NavigationBarItemDefaults.colors (
+                        indicatorColor = Color.Red
+                    ),
+                )
+                NavigationBarItem(
+                    selected = activeScreen == "Stats",
+                    onClick = { onScreenChange("Stats") },
+                    icon = { Icon(painter = painterResource(R.drawable.stats), contentDescription = "Stats", modifier = Modifier.size(20.dp)) },
+                    label = { Text("Stats") },
+                    colors = NavigationBarItemDefaults.colors (
+                        indicatorColor = Color.Red
+                    ),
+                )
+                NavigationBarItem(
+                    selected = activeScreen == "Schedule",
+                    onClick = { onScreenChange("Schedule") },
+                    icon = { Icon(painter = painterResource(R.drawable.calendar), contentDescription = "Schedule", modifier = Modifier.size(20.dp)) },
+                    label = { Text("Schedule") },
+                    colors = NavigationBarItemDefaults.colors (
+                        indicatorColor = Color.Red
+                    ),
+                )
+                NavigationBarItem(
+                    selected = activeScreen == "Favourite",
+                    onClick = { onScreenChange("Favourite") },
+                    icon = { Icon(painter = painterResource(R.drawable.favorite), contentDescription = "Favourite", modifier = Modifier.size(20.dp)) },
+                    label = { Text("Favourite") },
+                    colors = NavigationBarItemDefaults.colors (
+                        indicatorColor = Color.Red
+                    ),
+                )
             }
         }
+    ) { innerPadding ->
+            Box(
+                modifier
+                    .fillMaxSize()
+                    .background(PitWallBackground)
+                    .padding(top = innerPadding.calculateTopPadding()),
+            ) {
+                when (activeScreen) {
+                    "Home" -> HomeScreen(modifier, onScreenChange = onScreenChange)
+                    "All Result" -> ResultScreen()
+                    "Stats" -> StatsScreen()
+                    "Schedule" -> ScheduleScreen()
+                    "Favourite" -> FavouriteScreen()
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(120.dp)
+                        .align(Alignment.BottomCenter)
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(Color.Transparent, PitWallBackground)
+                            )
+                        )
+                ) {}
+            }
+
+
     }
 }
 
